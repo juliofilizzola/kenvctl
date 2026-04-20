adce80f52a60b9604762e5609316c34d66680670
Julio Filizzola <juliofilizzola@hotmail.com>
2026-04-20 07:46:20 -0300

    feat: integrate environment creation in list command
    details:
    - call `CreateEnv` from service package within the `list` command
    - handle errors and print created environment details

--- internal/command/command.go
+++ internal/command/command.go
@@ -3,16 +3,23 @@ package command
 import (
 	"fmt"
 
+	"github.com/juliofilizzola/kenvctl/internal/service"
 	"github.com/spf13/cobra"
 )
 
 func Add(cmd *cobra.Command, args []string) (err error) {
 	fmt.Println("add called")
+
 	return nil
 }
 
 func List(cmd *cobra.Command, args []string) (err error) {
 	fmt.Println("list called")
+	env, err := service.CreateEnv()
+	if err != nil {
+		return err
+	}
+	fmt.Println(env)
 	return nil
 }
 
